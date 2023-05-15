29cd83b08c9c4fdc703d1be7debb7279af97bd55
Otaviowillemen <otaviowillemen.aprendiz@gmail.com>
2023-05-14 22:20:37 -0300

    Estudos

--- Mundo 1/app.py
+++
@@ -1,2 +0,0 @@
-print ('Olá mundo')
-idade = 18
---
+++ Mundo 1/ex003.py
@@ -0,0 +1,4 @@
+n1=int(input('digite um número : '))
+n2=int(input('digito outro número : '))
+soma=int(n1+n2)
+print('A soma entre {} e {} é igual á {}'.format(n1, n2, soma))
