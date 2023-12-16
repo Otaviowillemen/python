3d1f5958227f9e097175045c85ebd84fdd11b27f
Otávio Willemen <otaviowillemen.aprendiz@gmail.com>
2023-12-16 17:12:14 -0300

    2

---
+++ mundo 2/ex013.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro: '))
+n3 = n1*n2
+print(n3)
+n4 = int(input('por quanto deseja dividir esse número: '))
+n5 = n3/n4
+print('o resultado é ', n5)
